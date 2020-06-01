c03d1c9c49a1edec23b1cef5b1e45d16faeb1102
Sean Mufarinya <seanmuf@gmail.com>
2020-06-01 01:11:59 -0400

    search services start

--- src/services/SearchServices.js
+++ src/services/SearchServices.js
@@ -2,3 +2,12 @@
 const findProductsByKeyWord = (keyword) =>
     fetch('')
         .then(response => response.json())
+
+const findpizza = (keyword) =>
+    fetch('')
+        .then(response => response.json())
+
+export default {
+    findProductsByKeyWord,
+    findpizza
+}
