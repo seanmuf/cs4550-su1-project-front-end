4d5aaad563f79397f84b2dbd0a6dae35ab90478f
Sean Mufarinya <seanmuf@gmail.com>
2020-06-01 01:17:28 -0400

    api client component start

---
+++ src/components/ApiClientComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchContainer from "../containers/SearchContainer";
+
+export default class ApiClientComponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <SearchContainer/>
+            </div>
+        )
+    }
+
+}
