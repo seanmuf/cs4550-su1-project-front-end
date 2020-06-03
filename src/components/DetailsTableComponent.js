61ec2984fb8f0a8de2fc36602b9de4f03a85a662
Sean Mufarinya <seanmuf@gmail.com>
2020-06-03 00:34:45 -0400

    implemented rendering

--- src/components/DetailsTableComponent.js
+++ src/components/DetailsTableComponent.js
@@ -1,50 +1,31 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import SearchServices from "../services/SearchServices";
+import DetailsRowComponent from "./DetailsRowComponent";
+import SearchCardComponent from "./SearchCardComponent";
 
-export default class DetailsComponent extends React.Component {
-
-    state = {
-        product: this.props.product,
-        productId: this.props.match.params.productId
-    }
-
-    componentDidMount() {
-        SearchServices.findProductById(this.props.match.params.productId)
-            .then(actualProduct => this.setState({
-                product: actualProduct
-            }))
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.match.params.productId !== this.props.match.params.productId) {
-            this.setState({
-                productId: this.props.match.params.productId
-            })
-        }
-    }
-
+export default class DetailsTableComponent extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar fixed-top navbar-dark bg-dark">
-                    <div>
-                        <Link to="/search">Back To Search</Link>
-                    </div>
-                    <div className="navbar-brand mr-auto detailsPageHeader">
-                        <h3>Details Page</h3>
-                    </div>
-                </nav>
-
                 <table className="table detailsTable">
                     <thead className="detailsTableHeader">
                         <tr>
+                            <td>Image</td>
                             <td>Title</td>
                             <td>Price</td>
+                            <td>Rating</td>
+                            <td>Total Reviews</td>
+                            <td>Link To Product</td>
                         </tr>
                     </thead>
                     <tbody>
-
+                    {
+                        this.props.product.map(product =>
+                                <DetailsRowComponent
+                                    product={product}
+                                    key={this.props.product.ASIN}
+                                />
+                        )
+                    }
                     </tbody>
                 </table>
             </div>
