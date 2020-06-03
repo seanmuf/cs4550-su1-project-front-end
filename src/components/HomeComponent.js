6170a5aa293f602b698a073398b808d9e4fb8aae
Sean Mufarinya <seanmuf@gmail.com>
2020-06-03 00:38:21 -0400

    added links/rendering changes

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,18 +3,18 @@ import {Link} from "react-router-dom";
 
 const HomeComponent = () =>
     <div>
-        <div>
+        <div className="container-fluid">
             <nav className="navbar fixed-top navbar-dark bg-dark">
                 <div className="navbar-brand mx-auto">
                     <h3>Student MarketPlace</h3>
                 </div>
                 <ul className="nav navbar-nav" style={{flexDirection: 'row'}}>
                     <li>
-                        <Link className="nav-link" to="/search">Login</Link>
+                        <Link className="nav-link" to="/login">Login</Link>
                     </li>
                     <li className="loginDivider">/</li>
                     <li>
-                        <Link className="nav-link" to="/search">Sign Up</Link>
+                        <Link className="nav-link" to="/register">Register</Link>
                     </li>
                 </ul>
             </nav>
